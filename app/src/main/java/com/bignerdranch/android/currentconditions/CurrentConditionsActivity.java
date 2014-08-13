ef279d2eb577e81469e411e3916acc376b178e1c
Brad Schricker <brad@bignerdranch.com>
2014-08-13 14:27:32 -0400

    Added support for celcius temperature readings.

--- app/src/main/java/com/bignerdranch/android/currentconditions/CurrentConditionsActivity.java
+++ app/src/main/java/com/bignerdranch/android/currentconditions/CurrentConditionsActivity.java
@@ -1,19 +1,47 @@
 package com.bignerdranch.android.currentconditions;
 
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
-public class CurrentConditionsActivity extends Activity {
+public class CurrentConditionsActivity extends Activity implements SensorEventListener {
+
+    private TextView mTimeTextView;
+    private TextView mTemperatureTextView;
+    private TextView mHumidityTextView;
+    private TextView mPressureTextView;
+
+    private SensorManager mSensorManager;
+    private Sensor mThermometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_conditions);
+
+        mTimeTextView = (TextView) findViewById(R.id.time_text_view);
+        mTemperatureTextView = (TextView) findViewById(R.id.temperature_text_view);
+        mHumidityTextView = (TextView) findViewById(R.id.humidity_text_view);
+        mPressureTextView = (TextView) findViewById(R.id.pressure_text_view);
+
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mThermometer = mSensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+
     }
 
+    @Override
+    protected void onResume() {
+        // Register a listener for the sensor.
+        super.onResume();
+        mSensorManager.registerListener(this, mThermometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,5 +60,16 @@ public class CurrentConditionsActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //  Do something if the accuracy changes
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() != Sensor.TYPE_AMBIENT_TEMPERATURE) return;
+
+        float temperature_celcius = event.values[0];
+        mTemperatureTextView.setText("" + temperature_celcius);
     }
 }
